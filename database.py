d98eaab31345d02c03a372d6746162e420869b58
deepak <67363220+deeppil@users.noreply.github.com>
2026-01-12 19:22:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb+srv://deepakpil2006_db_user:UZMdudfEWTVdtt7W@crudapp.epfnwri.mongodb.net/?appName=CRUDapp"
-client = MongoClient(MONGO_URL)
+MONGO_URL = os.getenv("MONGO_URL")
 
+client = MongoClient(MONGO_URL)
 db = client["student_db"]
 student_collection = db["students"]
